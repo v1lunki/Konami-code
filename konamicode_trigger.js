9dbf5d847b6c0ca64f2f3880debea12fbe569149
v1lunki <120061434+v1lunki@users.noreply.github.com>
2024-09-23 15:26:41 +0300

    Add files via upload

--- konamicode_trigger.js
+++ konamicode_trigger.js
@@ -1,54 +1,27 @@
-// <script src="jquery-3.7.1.min.js"></script>
+function konamicodeListener() {
+    // Declaring the comparison array
+    var keyCombinationArray = [];
+    const triggerCodeArray = ["ARROWUP", "ARROWUP", "ARROWDOWN", "ARROWDOWN", "ARROWLEFT", "ARROWRIGHT", "ARROWLEFT", "ARROWRIGHT", "B", "A"];
 
-$(document).ready(function () {
-    // Declaring the comparison string
-    var keyCombinationString = ""
-    var triggerCode = "UUDDLRLRBA"
-
-    // Event trigger for key that's pressed down. Picks up pressed key and adds it to string if it's one of the keys of Konami code
+    // Event trigger for key that's pressed down. Picks up pressed key and adds it to array
     $(document).keydown(function (event) {
-        var keyPressed = event.key.toUpperCase(); // Changes string to upper cases so that it wouldn't matter if caps lock is on or off
+        keyCombinationArray.push(event.key.toUpperCase()); // Changes the key string to upper cases and adds it to the array
 
-        switch (keyPressed) {
-            case "ARROWLEFT":
-                var keyToString = "L";
-                break;
-            case "ARROWRIGHT":
-                var keyToString = "R";
-                break;
-            case "ARROWUP":
-                var keyToString = "U";
-                break;
-            case "ARROWDOWN":
-                var keyToString = "D";
-                break;
-            case "A":
-                var keyToString = "A";
-                break;
-            case "B":
-                var keyToString = "B";
-                break;
-            default:
-                keyToString = "";
-                keyCombinationString = "";
-        }
-        keyCombinationString += keyToString;
+        // Cleans up the array in case the input varies from triggerCode
+        cleanupArray();
 
-        // Cleans up the string in case the input varies from triggerCode
-        keyCombinationString = cleanupString(keyCombinationString);
+        // Prints combination in console (for debug)
+        console.log(keyCombinationArray);
 
-        // Prints combination to screen (for debug)
-        $("#showstring").text(keyCombinationString);
-
-        // Makes a check to the input combination and triggers function if Konami code is triggered
-        if (isKonamicode(keyCombinationString)) {
+        // Checks if konami code is launched
+        if (isKonamicode()) {
             konamicodeTriggered();
         };
     });
 
     // Check if keyboard combination is Konami code
-    function isKonamicode(code) {
-        if (triggerCode.match(code) == triggerCode) {
+    function isKonamicode() {
+        if (keyCombinationArray.toString() == triggerCodeArray.toString()) {
             return true;
         } else {
             return false;
@@ -57,20 +30,18 @@ $(document).ready(function () {
 
     // Test function that is triggered by Konami code
     function konamicodeTriggered() {
-        $("#root").text("Konami code has been triggered!");
+        console.log("Konami code has been triggered!");
     }
 
-    // Cleans up the string (prevents that user can't form an endlessly long string)
-    function cleanupString(stringToclean) {
-        if (triggerCode.includes(stringToclean) == false) {
-            if (stringToclean.endsWith("U")) {
-                stringToclean = "U"
-            } else if (stringToclean.endsWith("UU")) {
-                stringToclean = "UU"
+    // Cleans up the array (prevents that user can't form an endlessly long array)
+    function cleanupArray() {
+        if (keyCombinationArray[keyCombinationArray.length - 1] != triggerCodeArray[keyCombinationArray.length - 1]) {
+            if (keyCombinationArray[keyCombinationArray.length - 1] == triggerCodeArray[0]) {
+                keyCombinationArray = []
+                keyCombinationArray.push(triggerCodeArray[0]);
             } else {
-                stringToclean = ""
+                keyCombinationArray = []
             }
         }
-        return stringToclean;
     }
-});
+}
--- konamicode_trigger.test.js
+++ konamicode_trigger.test.js
@@ -1,2 +1,6 @@
 const isKonamicode = require("/.konamicode_trigger");
-const cleanupString = require("/.konamicode_trigger");
+const cleanupArray = require("/.konamicode_trigger");
+
+test('compares non-Konami code to konami code'), () => {
+    expect
+}
